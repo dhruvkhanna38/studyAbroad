f18d97785910ed87d7da7f00873883a0e90ac8ad
Khanna, Dhruv <dhruvkhanna38@gmail.com>
2020-04-19 15:48:30 +0530

    email changed

--- emails/account.js
+++ emails/account.js
@@ -1,6 +1,7 @@
 const sgMail = require("@sendgrid/mail");
 
 const sendGridAPIKey = process.env.SENDGRID_API_KEY;
+console.log(sendGridAPIKey);
 
 sgMail.setApiKey(sendGridAPIKey);
 
@@ -8,7 +9,7 @@ sgMail.setApiKey(sendGridAPIKey);
 const sendEnquiryEmail = (name , email , subject , message)=>{
     sgMail.send({
         to : "dhruvkhanna38@gmail.com" , 
-        from :"dhruvkhanna38@gmail.com",
+        from :"ahuja.bhavat123@gmail.com",
         subject : subject, 
         text:`Hi! Dhruv, You have a new enquiry from ${name}.
         Email: ${email}
